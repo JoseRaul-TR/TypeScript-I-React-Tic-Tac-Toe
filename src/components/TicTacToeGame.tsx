4060b5b14d31cd51daa173c4899c9ba0bc3e68a6
José_Raúl Tenza_Ramírez <jraultr@gmail.com>
2025-09-02 08:59:46 +0200

    refactor game logic

--- src/components/TicTacToeGame.tsx
+++ src/components/TicTacToeGame.tsx
@@ -1,32 +1,6 @@
 import type { Cell, Player } from "../types/types";
+import { checkWin, checkDraw } from "../utils/gameLogic";
 import { useState } from "react";
-
-// Winning patterns
-const winPatterns: number[][] = [
-  // rows
-  [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8],
-  // columns
-  [0, 3, 6],
-  [1, 4, 7],
-  [2, 5, 8],
-  // diagonals
-  [0, 4, 8],
-  [2, 4, 6],
-];
-
-// Function to check winner
-function checkWin(board: Cell[], player: Player): boolean {
-  return winPatterns.some((pattern) =>
-    pattern.every((index) => board[index] === player)
-  );
-}
-
-// Function to check draw
-function checkDraw(board: Cell[]): boolean {
-  return board.every((cell) => cell !== null);
-}
 
 export default function TicTacToeGame() {
   const [board, setBoard] = useState<Cell[]>(Array(9).fill(null));
